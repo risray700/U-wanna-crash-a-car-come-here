ac4aa22d8f64409c9d540c24003858435d9a16a2
risray700 <74744721+risray700@users.noreply.github.com>
2020-12-17 18:12:20 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# U-wanna-crash-a-car-come-here
+# p5.play-boilerplate
+Boiler plate for p5.play
--- sketch.js
+++ sketch.js
@@ -1,40 +1,41 @@
-var car, wall;
-var speed, weight;
+var car,wall ; 
+var speed,weight ; 
 
 function setup() {
-  createCanvas(1600,400);
-speed=random(22,100)
-weight=random(400,6000)
+ var canvas =  createCanvas(800,400);
+ //canvas.shapeColor = color(80,80,80);
 
- car=createSprite(50, 200, 50, 50);
-car.velocityX = speed;
 
-wall=createSprite(1500,200,60,height/2);
-wall.shapeColor=(80,80,80);
+  speed=random(55,90);
+  weight=random(400,1500);
 
+ car =  createSprite(50,200,50,50);
+ 
+ wall = createSprite(400,200,60,120);
+ wall.shapeColor = color (80,80,80);
 }
+
 
 function draw() {
-  background(255,255,255);
+  background(80,80,80);  
 
-if(wall.x-car.x < (car.width+wall.width)/2)
-{
-  car.velocityX=0;
-  var deformation=0.5 * weight * speed* speed/22509;
-  if(deformation>180)
-  {
-    car.shapeColor=color(230,230,0);
-  }
+  car.velocityX = speed ; 
 
-
-  if(deformation<180 && deformation>80) {
-  car.shapeColor=color(220,20,60);
-}
-
-  if(deformation<90)
-  {
-    car.shapeColor=color(0,250,0);
-  }
-  }
+  if(wall.x - car.x < wall.width/2 + car.width/2) {
+      car.velocityX = 0 ;
+      var deformation = 0.5*weight*speed*speed /22500 ; 
+      if(deformation > 180 )
+      {
+        car.shapeColor = color(255,0,0);
+      }
+      if(deformation < 180 && deformation  > 100 ) 
+      {
+        car.shapeColor = color(230,230,0);
+      }
+      if(deformation < 100 ) 
+      {
+        car.shapeColor = color(0,255,0);
+      }
+    } 
   drawSprites();
 }
